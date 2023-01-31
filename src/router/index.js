d8fa6727e8c5f05f8e758eb3c71551bcc1fa202a
Liu Jie <1765659336@qq.com>
2023-01-31 14:09:20 +0800

    add:前置路由守卫，前端后端一起鉴权

--- src/router/index.js
+++ src/router/index.js
@@ -4,7 +4,12 @@ import Root from "@/views/root/Root.vue";
 import Home from "@/views/root/home/Home.vue";
 import PackagesDemo from "@/views/root/packages-demo/PackagesDemo.vue";
 Vue.use(VueRouter);
-
+/* 
+  meta: {
+    keepAlive:是否缓存组件
+    isAuth:前端设置是否需要跳过后端路由鉴权
+  }
+*/
 const routes = [
   {
     path: "/",
@@ -17,6 +22,7 @@ const routes = [
       import(/* webpackChunkName: "about" */ "../views/login/Login.vue"),
     meta: {
       keepAlive: false,
+      isAuth: true
     },
   },
   {
@@ -34,6 +40,7 @@ const routes = [
         component: Home,
         meta: {
           keepAlive: true,
+          isAuth: true
         },
       },
       {
@@ -42,6 +49,7 @@ const routes = [
         component: PackagesDemo,
         meta: {
           keepAlive: true,
+          isAuth: true
         },
       },
     ],
@@ -53,8 +61,18 @@ const router = new VueRouter({
 });
 
 router.beforeEach((to, from, next) => {
-  console.log(to, from, next);
-  next();
+  // 前端限制,isAuth为false或者没有设置，则代表没有权限
+  if (!to.meta.isAuth) {
+    // 后端限制,调用后端权限接口,判断是否有权限...
+    const auth = false;
+    if (auth) {
+      next();
+    } else {
+      alert("你没有访问权限");
+    }
+  } else {
+    next();
+  }
 });
 
 export default router;
