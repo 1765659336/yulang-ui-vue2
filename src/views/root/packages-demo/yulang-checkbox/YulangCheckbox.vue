4f51af2915221c4142aab4a36bafacf76201fa02
lang <2301619815@qq.com>
2023-02-06 15:01:05 +0800

    fix:修复Checkbox文档错别字

--- src/views/root/packages-demo/yulang-checkbox/YulangCheckbox.vue
+++ src/views/root/packages-demo/yulang-checkbox/YulangCheckbox.vue
@@ -14,7 +14,7 @@
           <yulang-button @click="btn">按钮</yulang-button>
 
           <template #tip>
-            <div>v=model绑定的是是否选中</div>
+            <div>v-model绑定的是是否选中</div>
             <div>label属性是左边的属性名称</div>
             <div>disabled属性表示是否不可用</div>
           </template>
@@ -33,7 +33,7 @@ export default {
     return {
       isCheckout: false,
       slotArr: [
-        { slotName: 'a', slotTitle: 'BackTop 回到顶部', level: 1 },
+        { slotName: 'a', slotTitle: 'Checkbox 复选框', level: 1 },
         { slotName: 'b', slotTitle: '基本使用', level: 2 },
       ],
       codeStr,
