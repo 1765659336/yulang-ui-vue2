dc7132e74d75a740e6f4037db72ba5d88b9ab92a
Liu Jie <1765659336@qq.com>
2023-02-22 22:15:36 +0800

    add : 骨架屏组件支持动画效果配置项

--- src/views/root/packages-demo/yulang-skeleton/components/SkeletonC.vue
+++ src/views/root/packages-demo/yulang-skeleton/components/SkeletonC.vue
@@ -1,21 +1,22 @@
 <template>
   <div>
-    <div class="skeleton-b-container" id="skeletonBContainer">
-      <div class="item item1">爱国</div>
-      <div class="item item2">进步</div>
-      <div class="item item3">民主</div>
-      <div class="item item4">科学</div>
-      <div class="item item5">敬业</div>
+    <div class="skeleton-b-container" id="skeletonBContainer2">
+      <div class="item item11">爱国</div>
+      <div class="item item22">进步</div>
+      <div class="item item33">民主</div>
+      <div class="item item44">科学</div>
+      <div class="item item55">敬业</div>
       <yulang-skeleton
         v-if="isShowSkeleton"
-        fatherDomSelect="#skeletonBContainer"
+        fatherDomSelect="#skeletonBContainer2"
         :skeletonArr="[
-          '.item1',
-          '.item2',
-          '.item3',
-          '.item4',
-          '.item5',
+          '.item11',
+          '.item22',
+          '.item33',
+          '.item44',
+          '.item55',
         ]"
+        animated
       ></yulang-skeleton>
     </div>
     <yulang-button @click="isShowSkeleton = !isShowSkeleton">{{
@@ -45,27 +46,27 @@ export default {
     margin: 10px 0;
     border: 1px solid red;
   }
-  .item1 {
+  .item11 {
     width: 500px;
     height: 25px;
   }
 
-  .item2 {
+  .item22 {
     width: 400px;
     height: 25px;
   }
 
-  .item3 {
+  .item33 {
     width: 300px;
     height: 25px;
   }
 
-  .item4 {
+  .item44 {
     width: 200px;
     height: 50px;
   }
 
-  .item5 {
+  .item55 {
     width: 600px;
     height: 50px;
   }
