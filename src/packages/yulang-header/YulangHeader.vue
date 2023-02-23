9b80150b1bde1cd8ad2a3b4dbabafebd5888bcbc
JIE <1765659336@qq.com>
2023-02-23 23:29:18 +0800

    add : 新增Container布局组件

---
+++ src/packages/yulang-header/YulangHeader.vue
@@ -0,0 +1,31 @@
+<template>
+  <div
+    class="yulang-header-container"
+    :style="{ '--yulang-header-height--': height }"
+  >
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "yulang-header",
+  props: {
+    height: {
+      type: String,
+      default: () => {
+        return "60px";
+      },
+    },
+  },
+  data() {
+    return {
+      yulangComponentName: "yulang-header",
+    };
+  },
+};
+</script>
+
+<style lang="less" scoped>
+@import url("./index.less");
+</style>
---
+++ src/views/root/packages-demo/yulang-container/data.js
@@ -0,0 +1,62 @@
+export const codeStrB = `
+<template>
+    <div>
+        <yulang-container>
+      <yulang-header>Header</yulang-header>
+      <yulang-main>Main</yulang-main>
+    </yulang-container>
+    <yulang-container>
+      <yulang-header>Header</yulang-header>
+      <yulang-main>Main</yulang-main>
+      <yulang-footer>Footer</yulang-footer>
+    </yulang-container>
+    <yulang-container>
+      <yulang-aside width="200px">Aside</yulang-aside>
+      <yulang-main>Main</yulang-main>
+    </yulang-container>
+    <yulang-container>
+      <yulang-header>Header</yulang-header>
+      <yulang-container>
+        <yulang-aside width="200px">Aside</yulang-aside>
+        <yulang-main>Main</yulang-main>
+      </yulang-container>
+    </yulang-container>
+    <yulang-container>
+      <yulang-header>Header</yulang-header>
+      <yulang-container>
+        <yulang-aside width="200px">Aside</yulang-aside>
+        <yulang-container>
+          <yulang-main>Main</yulang-main>
+          <yulang-footer>Footer</yulang-footer>
+        </yulang-container>
+      </yulang-container>
+    </yulang-container>
+    <yulang-container>
+      <yulang-aside width="200px">Aside</yulang-aside>
+      <yulang-container>
+        <yulang-header>Header</yulang-header>
+        <yulang-main>Main</yulang-main>
+      </yulang-container>
+    </yulang-container>
+
+    <yulang-container>
+      <yulang-aside width="200px">Aside</yulang-aside>
+      <yulang-container>
+        <yulang-header>Header</yulang-header>
+        <yulang-main>Main</yulang-main>
+        <yulang-footer>Footer</yulang-footer>
+      </yulang-container>
+    </yulang-container>
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="less" scoped>
+
+</style>
+`
