e2780b287e30f5788b6f44879246f9b5fce393ba
Liu Jie <1765659336@qq.com>
2023-02-23 22:46:29 +0800

    add : Radio单选框组件与RadioGroup单选框组组件

---
+++ src/packages/yulang-radio-group/YulangRadioGroup.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="packages-yulang-radio-group-container">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+import { uuid } from "@/packages/lib";
+export default {
+  name: "yulang-radio-group",
+  props: {
+    value: {},
+  },
+  data() {
+    return {
+      yulangComponentName: "yulang-radio-group",
+    };
+  },
+  computed: {
+    valueComputed: {
+      get() {
+        return this.value;
+      },
+      set(value) {
+        this.$emit("input", value);
+      },
+    },
+  },
+  mounted() {
+    // 为group中的所有radio添加相同的name
+    const uuidKey = uuid();
+    this.$children.forEach((component) => {
+      if (component.yulangComponentName === "yulang-radio") {
+        component.name = uuidKey;
+      }
+    });
+  },
+};
+</script>
+
+<style lang="less" scoped>
+@import url("./index.less");
+</style>
---
+++ src/views/root/packages-demo/yulang-radio/components/RadioC.vue
@@ -0,0 +1,19 @@
+<template>
+  <yulang-radio-group v-model="radio">
+    <yulang-radio label="1" disabled>Option 1</yulang-radio>
+    <yulang-radio label="2">Option 2</yulang-radio>
+    {{ radio }}
+  </yulang-radio-group>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      radio: null,
+    };
+  },
+};
+</script>
+
+<style lang="less" scoped></style>
