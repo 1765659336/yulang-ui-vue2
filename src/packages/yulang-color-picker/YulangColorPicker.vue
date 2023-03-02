91a78072a46b837c62a40676aea975f857150bea
Liu Jie <1765659336@qq.com>
2023-03-02 21:34:27 +0800

    add: 颜色选择器部分代码

--- src/packages/yulang-color-picker/YulangColorPicker.vue
+++ src/packages/yulang-color-picker/YulangColorPicker.vue
@@ -1,18 +1,20 @@
 <template>
   <div
     class="yulang-color-picker-container"
-    :style="{ '--color-block-background-color--': 'green' }"
+    :style="{ '--color-block-background-color--': valueComputed }"
   >
     <yulang-popover trigger="click" placement="bottom-start">
       <template #reference>
         <div class="reference-container">
           <div class="color-block">
-            <i class="iconfont icon-chevron-down"></i>
-            <!-- <i class="iconfont icon-guanbi"></i> -->
+            <i class="iconfont icon-guanbi" v-if="valueComputed"></i>
+            <i class="iconfont icon-chevron-down" v-else></i>
           </div>
         </div>
       </template>
-      <template #content> hahaha </template>
+      <template #content>
+        <div class="color-picker-popover-container"></div>
+      </template>
     </yulang-popover>
   </div>
 </template>
@@ -23,6 +25,19 @@ export default {
   name: "yulang-color-picker",
   components: {
     YulangPopover,
+  },
+  props: {
+    value: {},
+  },
+  computed: {
+    valueComputed: {
+      get() {
+        return this.value;
+      },
+      set(value) {
+        this.$emit("input", value);
+      },
+    },
   },
 };
 </script>
--- src/views/root/packages-demo/yulang-color-picker/YulangColorPicker.vue
+++ src/views/root/packages-demo/yulang-color-picker/YulangColorPicker.vue
@@ -1,10 +1,15 @@
 <template>
-  <yulang-color-picker></yulang-color-picker>
+  <yulang-color-picker v-model="value"></yulang-color-picker>
 </template>
 
 <script>
 export default {
   name: "packages-demo-yulang-color-picker",
+  data() {
+    return {
+      value: 'red',
+    };
+  },
 };
 </script>
 
