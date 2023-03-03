ea25ff6992eb1615bdbb6663ff81f20ed9346f91
JIE <1765659336@qq.com>
2023-03-03 22:53:51 +0800

    add: 抖动离开动画

---
+++ src/views/root/resources/animate/components/bounce-out/BounceOutBottom.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="yulang-animate-div yulang-animate yulang-bounce-out-bottom">
+    抖动掉落离开
+  </div>
+</template>
+
+<script>
+import { disappear } from "../mixin";
+
+export default {
+  name: "yulang-bounce-out-bottom",
+  mixins: [disappear],
+  data() {
+    return {
+      title: "抖动掉落离开",
+    };
+  },
+};
+</script>
+
+<style lang="less" scoped></style>
