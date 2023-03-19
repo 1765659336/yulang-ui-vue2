86f923376138c25ee4cc186fa57ad3c308c5f6ec
JIE <1765659336@qq.com>
2023-03-19 23:01:52 +0800

    add: 时间选择器时间范围选择

--- src/views/root/packages-demo/yulang-time-picker/YulangTimePicker.vue
+++ src/views/root/packages-demo/yulang-time-picker/YulangTimePicker.vue
@@ -6,23 +6,37 @@
         <time-picker-b></time-picker-b>
       </yulang-describe-frame>
     </template>
+    <template #c>
+      可以选择任意时间
+      <yulang-describe-frame :codeStr="codeStrC">
+        <time-picker-c></time-picker-c>
+        <template #tip>
+          使用 el-time-picker 标签，通过selectableRange限制可选时间范围
+        </template>
+      </yulang-describe-frame>
+    </template>
   </yulang-anchor-point>
 </template>
 
 <script>
 import TimePickerB from "./components/TimePickerB.vue";
-import { codeStrB } from "./data";
+import TimePickerC from "./components/TimePickerC.vue";
+
+import { codeStrB, codeStrC } from "./data";
 export default {
   name: "packages-demo-yulang-time-picker",
   components: {
     TimePickerB,
+    TimePickerC,
   },
   data() {
     return {
       codeStrB,
+      codeStrC,
       slotArr: [
         { slotName: "a", slotTitle: "TimePicker 时间选择器", level: 1 },
         { slotName: "b", slotTitle: "固定时间点", level: 2 },
+        // { slotName: "c", slotTitle: "任意时间点", level: 2 },
       ],
     };
   },
--- src/views/root/packages-demo/yulang-time-picker/data.js
+++ src/views/root/packages-demo/yulang-time-picker/data.js
@@ -23,3 +23,29 @@ export default {
 
 <style lang="less" scoped></style>
 `;
+
+export const codeStrC = `
+<template>
+  <yulang-time-picker
+    arrow-control
+    v-model="value"
+    :picker-options="{
+      selectableRange: '18:30:00 - 20:30:00',
+    }"
+    placeholder="任意时间点"
+  >
+  </yulang-time-picker>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      value: new Date(2016, 9, 10, 18, 40),
+    };
+  },
+};
+</script>
+
+<style lang="less" scoped></style>
+`
